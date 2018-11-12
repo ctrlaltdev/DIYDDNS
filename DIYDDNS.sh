812c772225be503f2c59052b84b56cf0904cf493
ctrlaltdev <yorick@ctrlaltdev.xyz>
2018-11-12 13:20:49 -0800

    At the beginning was bash

---
+++ DIYDDNS.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+source ./.env
+
+DIGIP=$(dig +short $DNS_NAME)
+WANIP=$(dig +short myip.opendns.com @resolver1.opendns.com)
+
+if [ "$DIGIP" != "$WANIP" ]
+then
+  curl -X PUT "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$DNS_ID" \
+    -H "X-Auth-Email: $EMAIL" \
+    -H "X-Auth-Key: $CLOUDFLARE_API_KEY" \
+    -H "Content-Type: application/json" \
+    --data '{"type":"A","name":"'"$DNS_NAME"'","content":"'"$WANIP"'","proxied":"false","ttl":"1"}' \
+    --silent
+fi
